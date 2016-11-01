90d000526966ef4dbbc7700cad4ca1a527ac873b
Combii <davidstovle@gmail.com>
2016-11-01 13:23:55 +0100

    Added files to Data and Logic so they aren't empty

---
+++ src/Data/Test.java
@@ -0,0 +1,7 @@
+package Data;
+
+/**
+ * Created by David Stovlbaek on 01/11/2016.
+ */
+public class Test {
+}
---
+++ src/Logic/Test.java
@@ -0,0 +1,7 @@
+package Logic;
+
+/**
+ * Created by David Stovlbaek on 01/11/2016.
+ */
+public class Test {
+}
